1048c2af14a30565714ee24f0095a3864fef19fe
salmanshahid5 <salmanshahid0333@gmail.com>
2024-09-19 15:32:01 +0500

    create form section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+export default App;
+
+// logo section
+
+function Logo() {
+  return <h1 className="logo">🌳 Far Away 👜</h1>;
 }
 
-export default App
+// form section
+
+function Form() {
+
+  const [inputText,SetInputText] = useState('')
+  const [itemNumber,SetItemNumber] = useState(1)
+
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+      <select value={itemNumber} onChange={(e) => SetItemNumber(Number(e.target.value))}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((elem) => (
+          <option key={elem}>{elem}</option>
+        ))}
+      </select>
+      <input onChange={(e)=> SetInputText(e.target.value)} id="add" placeholder="Item Name" value={inputText}></input>
+      <button className="add-btn">Add</button>
+    </div>
+  );
+}
+
+
